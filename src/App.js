6e2c1a73302f3cec8f10518aa8f5185ad696c2db
namansharma3007 <96349715+namansharma3007@users.noreply.github.com>
2023-07-21 08:07:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './Style.css';
 import React, { useState, useEffect } from 'react';
-import { MdOutlineDeleteForever } from 'react-icons/md'
+import { MdOutlineDeleteForever } from 'react-icons/md';
 import { BsCheckLg } from 'react-icons/bs'
+import { GiNetworkBars } from 'react-icons/gi';
+import { BiSolidPencil } from 'react-icons/bi';
 
 function TaskForm({ addTask }) {
   const [task, setTask] = useState('');
@@ -27,26 +29,56 @@ function TaskForm({ addTask }) {
   );
 }
 
-function TaskItem({ task, toggleTask, deleteTask }) {
+function TaskItem({ task, toggleTask, deleteTask, editTask }) {
+  const [editMode, setEditMode] = useState(false);
+  const [editedTitle, setEditedTitle] = useState('');
+
+  const handleEdit = () => {
+    setEditedTitle(task.title);
+    setEditMode(true);
+  };
+
+  const handleEditSave = () => {
+    editTask(task.id, editedTitle);
+    setEditMode(false);
+  };
+
   return (
     <li>
-      <span
-        style={{
-          textDecoration: task.completed ? 'line-through' : 'none',
-        }}
-        onClick={() => toggleTask(task.id)}
-      >
-        {task.title}
-      </span>
-      <button onClick={() => deleteTask(task.id)} className='delete-btn'><MdOutlineDeleteForever /></button>
-      {!task.completed && (
-        <button onClick={() => toggleTask(task.id)} className='complete-btn'><BsCheckLg /></button>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => toggleTask(task.id)}
+      />
+      {!editMode ? (
+        <span
+          style={{
+            textDecoration: task.completed ? 'line-through' : 'none',
+          }}
+          onClick={() => toggleTask(task.id)}
+        >
+          {task.title}
+        </span>
+      ) : (
+        <input
+          type="text"
+          value={editedTitle}
+          onChange={(e) => setEditedTitle(e.target.value)}
+        />
+      )}
+      <button onClick={() => deleteTask(task.id)} className='delete-btn'>
+        <MdOutlineDeleteForever />
+      </button>
+      {editMode ? (
+        <button onClick={handleEditSave} className='edit-btn'><BsCheckLg/></button>
+      ) : (
+        <button onClick={handleEdit} className='edit-btn'><BiSolidPencil/></button>
       )}
     </li>
   );
 }
 
-function TaskList({ tasks, toggleTask, deleteTask }) {
+function TaskList({ tasks, toggleTask, deleteTask, editTask }) {
   return (
     <ul>
       {tasks.map((task) => (
@@ -55,6 +87,7 @@ function TaskList({ tasks, toggleTask, deleteTask }) {
           task={task}
           toggleTask={toggleTask}
           deleteTask={deleteTask}
+          editTask={editTask}
         />
       ))}
     </ul>
@@ -74,37 +107,37 @@ function FilterOptions({ filter, setFilter, tasks }) {
 
   return (
     <div className='filterSection'>
-        <input
-          type="radio"
-          name="filter"
-          value="all"
-          checked={filter === 'all'}
-          onChange={handleFilterChange}
-          id="all"
-        />
-      <label for="all">
+      <input
+        type="radio"
+        name="filter"
+        value="all"
+        checked={filter === 'all'}
+        onChange={handleFilterChange}
+        id="all"
+      />
+      <label htmlFor="all">
         All-{totalTasks}
       </label>
-        <input
-          type="radio"
-          name="filter"
-          value="completed"
-          checked={filter === 'completed'}
-          onChange={handleFilterChange}
-          id="completed"
-        />
-      <label for="completed">
+      <input
+        type="radio"
+        name="filter"
+        value="completed"
+        checked={filter === 'completed'}
+        onChange={handleFilterChange}
+        id="completed"
+      />
+      <label htmlFor="completed">
         Completed-{completed}
       </label>
-        <input
-          type="radio"
-          name="filter"
-          value="incomplete"
-          checked={filter === 'incomplete'}
-          onChange={handleFilterChange}
-          id="incomplete"
-        />
-      <label for="incomplete">
+      <input
+        type="radio"
+        name="filter"
+        value="incomplete"
+        checked={filter === 'incomplete'}
+        onChange={handleFilterChange}
+        id="incomplete"
+      />
+      <label htmlFor="incomplete">
         Incomplete-{incompleted}
       </label>
     </div>
@@ -153,6 +186,15 @@ function App() {
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
+  const editTask = (id, newTitle) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, title: newTitle };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
   const filteredTasks = tasks.filter((task) => {
     if (filter === 'completed') {
@@ -166,19 +208,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Task Manager</h1>
+      <h1>Task Manager <GiNetworkBars /></h1>
       <TaskForm addTask={addTask} />
       <FilterOptions filter={filter} setFilter={setFilter} tasks={tasks} />
       {
-        tasks.length > 0 ? 
-        <TaskList
-          tasks={filteredTasks}
-          toggleTask={toggleTask}
-          deleteTask={deleteTask}
-        /> :
-        <span className='no-task-notice'>Your task manager is empty.</span>
+        tasks.length > 0 ?
+          <TaskList
+            tasks={filteredTasks}
+            toggleTask={toggleTask}
+            deleteTask={deleteTask}
+            editTask={editTask}
+          /> :
+          <span className='no-task-notice'>Your task manager is empty.</span>
       }
-      <footer className='copyrightline'>Copyright &#169; Naman Sharma <a href='https://www.linkedin.com/in/naman-sharma-b46950226/' target='_blank' rel="noopener noreferrer">About Me</a>
+      <footer className='copyrightline'>
+        Copyright &#169; Naman Sharma <a href='https://www.linkedin.com/in/naman-sharma-b46950226/' target='_blank' rel="noopener noreferrer">About Me</a>
       </footer>
     </div>
   );
